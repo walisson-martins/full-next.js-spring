6b5fd397c9d701b49f448c026b127b7633c60cb1
Walisson Martins da Silva <walisson.silva@implantainformatica.com.br>
2021-12-01 18:39:55 -0300

    correções

---
+++ src/pages/meu-componente.tsx
@@ -0,0 +1,23 @@
+// function Mensagem(props: any) {
+// }
+
+// function MeuComponente() {
+// }
+
+interface MensagemProps {
+  mensagem: string;
+}
+
+const Mensagem: React.FC<MensagemProps> = (props: MensagemProps) => {
+  return <div>{props.mensagem}</div>;
+};
+
+const MeuComponente = () => {
+  return (
+    <div>
+      <Mensagem mensagem="Mudei a mensagem" />
+    </div>
+  );
+};
+
+export default MeuComponente;
